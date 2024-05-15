88d0b42e4302dc6035d1f21870d2c0a1955b0dc1
depanwitait41 <depanwita18@gmail.com>
2024-05-15 23:06:34 +0530

    'Dynamic turns'

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const board=[
-    [9747, 1054, 9747],
-    [9747, 1054, 1054],
-    [1054, 9747, 9747]
+//     [9747, 1054, 9747]
+[null, null, null],
+[null, null, null],
+[null, null, null]
 ]
 
 export const Gameboard = () => {
+    const [turns, setTurns] = useState(board)
+    const handleOnClick=(x_index, y_index)=>{
+        setTurns((prevState)=>{
+            const newTurns = [...prevState.map(row=> [...row])]
+            newTurns[x_index][y_index]= '*';
+            return newTurns;
+        })
+    }
   return (
     <ol id='game-board'>
-        {board.map((row)=>{
+        {turns.map((row, row_index)=>{
             return (
                 <li>
                     <ol>
-                        {row.map((col)=>
+                        {row.map((col, col_index)=>
                             {
                                 return (
                                     <li>
-                                        <div className='game-cell'>{String.fromCodePoint(col)}</div>
+                                        <div className='game-cell' onClick={()=>handleOnClick(row_index, col_index)}>{col}</div>
                                     </li>
                                 )
                             }
