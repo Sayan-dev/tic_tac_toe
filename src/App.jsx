16739e99e1b791b83aca47d64d6e126f19f26e14
depanwitait41 <depanwita18@gmail.com>
2024-05-19 22:57:34 +0530

    Binding all states up

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,39 @@ import { useState } from 'react';
 import { Gameboard } from './components/Gameboard.jsx';
 import Player from './components/Player.jsx';
 
+const board=[
+  //     [9747, 1054, 9747]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+  ]
+
 function App() {
   const [currentPlayer, setCurrentPlayer]=useState(0);
-  const handleCurrentPlayer=()=>{
-    setCurrentPlayer(currentPlayer === 0 ? 1: 0)
-  }
+  const [turns, setTurns] = useState([])
+  const gameBoard=[...board]
+    const handleOnClick=(x_index, y_index)=>{
+      if(gameBoard[x_index][y_index])
+        return;
+      setCurrentPlayer(currentPlayer === 0 ? 1: 0);
+        setTurns((prevState)=>{
+          let player_index=0
+          if(prevState.length>0 && prevState[prevState.length-1].playerInfo===0)
+            {player_index=1}
+            const newTurns = {
+              indexInfo:{
+                row: x_index,
+                col: y_index
+              },
+              playerInfo: player_index
+            };
+            return [...prevState, newTurns];
+        })
+    }
+    turns.map((turn)=>{
+      gameBoard[turn.indexInfo.row][turn.indexInfo.col]=(turn.playerInfo === 0 ? String.fromCodePoint(9747) : String.fromCodePoint(1054))
+    })
+  
   return (
     <main>
       <div id="game-container">
@@ -14,7 +42,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" currentPlayer={currentPlayer}/>
           <Player initialName="Player 2" symbol="O" currentPlayer={currentPlayer} />
         </ol>
-        <Gameboard handleCurrentPlayer={handleCurrentPlayer} currentPlayer={currentPlayer}/>
+        <Gameboard handleOnClick={handleOnClick} gameBoard={gameBoard}/>
       </div>
       LOG
     </main>
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,28 +1,9 @@
 import React, { useState } from 'react'
 
-const board=[
-//     [9747, 1054, 9747]
-[null, null, null],
-[null, null, null],
-[null, null, null]
-]
-
-export const Gameboard = ({currentPlayer ,handleCurrentPlayer}) => {
-
-    const [turns, setTurns] = useState(board)
-    const handleOnClick=(x_index, y_index)=>{
-        handleCurrentPlayer()
-        setTurns((prevState)=>{
-            const newTurns = [...prevState.map(row=> [...row])]
-            if(!newTurns[x_index][y_index]){
-                newTurns[x_index][y_index]= (currentPlayer=== 0 ? String.fromCodePoint(9747) : String.fromCodePoint(1054));
-            }
-            return newTurns;
-        })
-    }
+export const Gameboard = ({handleOnClick, gameBoard}) => {
   return (
     <ol id='game-board'>
-        {turns.map((row, row_index)=>{
+        {gameBoard.map((row, row_index)=>{
             return (
                 <li>
                     <ol>
